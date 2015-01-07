62798a4b3a764dc87735ae09c1df7c1de13ede05
Sasha <sasha@webfactory.mk>
2015-01-07 12:27:43 +0100

    Container look and feel

--- Scrambled/SCBContainerViewController.m
+++ Scrambled/SCBContainerViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SCBContainerViewController.h"
+#import "SCBSettingsViewController.h"
 #import <Masonry/Masonry.h>
 
 @interface SCBContainerViewController ()
@@ -22,14 +23,40 @@
     UIImageView* backgroundImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background"]];
     backgroundImage.contentMode = UIViewContentModeScaleAspectFill;
     [self.view addSubview:backgroundImage];
+    
+    UIView *bottomBar = [UIView new];
+    [self.view addSubview:bottomBar];
+    bottomBar.backgroundColor = [UIColor grayColor];
+    
+    UIButton *settingsButton = [UIButton buttonWithType:(UIButtonTypeSystem)];
+    [settingsButton setImage:[UIImage imageNamed:@"settings"] forState:(UIControlStateNormal)];
+    settingsButton.adjustsImageWhenDisabled = YES;
+    [settingsButton addTarget:self action:@selector(onSettingsButton:) forControlEvents:(UIControlEventTouchUpInside)];
+    settingsButton.adjustsImageWhenDisabled = NO;
+    [bottomBar addSubview:settingsButton];
+    
+    [settingsButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.equalTo(settingsButton.mas_width);
+        make.height.equalTo(bottomBar);
+        make.centerY.equalTo(bottomBar);
+        make.right.equalTo(bottomBar).offset(-5.0);
+    }];
+    
     [backgroundImage mas_makeConstraints:^(MASConstraintMaker *make) {
         make.edges.equalTo(self.view);
     }];
+
+    [bottomBar mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.bottom.equalTo(self.view);
+        make.height.equalTo(@40);
+    }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Actions
+
+- (void)onSettingsButton:(UIButton *)sender
+{
+    
 }
 
 @end
